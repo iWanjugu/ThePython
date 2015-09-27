b398b40c422f99fd188343ac0de0a22fef1ea8eb
Wanjugu Irene <ireneemaina@gmail.com>
2015-09-27 18:18:37 +0300

    Joining strings to other strings

---
+++ string-concatenation.py
@@ -0,0 +1,17 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 3+4
+7
+>>> "sue is brilliant"
+'sue is brilliant'
+>>> print ("grow")
+grow
+>>> firstname = "Irene"
+>>> firstname + "Wanjugu"
+'IreneWanjugu'
+>>> firstname + " Maina"
+'Irene Maina'
+>>> firstname * 5
+'IreneIreneIreneIreneIrene'
+>>> #ppp
+>>>
