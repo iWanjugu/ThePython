fac70153b70f5977ff0914f45889dabb544e708f
Wanjugu Irene <ireneemaina@gmail.com>
2015-09-27 20:17:55 +0300

    If statements

---
+++ if.py
@@ -0,0 +1,20 @@
+__author__ = 'iWanjugu'
+# if statement for numbers
+age = 13 #= sign for
+
+if age < 21: # colon acts like brackets
+    print("No beer for you!")
+
+# if statement for string
+name = "Ben"
+
+if name is "Bucky": # acts as an 'equals' sign
+    print("Hey there Bucky")
+elif name is "Sam":
+    print("Yo!")
+elif name is "Lucy":
+    print("Yo Luce!")
+else:
+    print("please sign up for the site")
+
+
