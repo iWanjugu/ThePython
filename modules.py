b80b2b9b33020585e9ae5333f038c1f3914187cb
Wanjugu Irene <ireneemaina@gmail.com>
2015-09-28 04:49:31 +0300

    Using modules in other files

---
+++ modules.py
@@ -0,0 +1,11 @@
+__author__ = 'iWanjugu'
+
+#For functions that you'll use over and over again - created and stored in another file that will be used/called in other files
+#importing from the 'functions' file
+
+import functions
+import random #functions not built in to the core python things
+
+functions.beef()
+x = random.randrange(1,1000) #generates a random number between the specified rannge
+print (x)
