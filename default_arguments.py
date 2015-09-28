c5ce633406fee2eb21b64f0a53866b45a232f8f0
Wanjugu Irene <ireneemaina@gmail.com>
2015-09-28 03:11:15 +0300

    Setting default funtions for arguments in functions

---
+++ default_arguments.py
@@ -0,0 +1,13 @@
+__author__ = 'iWanjugu'
+#Setting default values for the arguments
+
+def get_gender (sex='Unknown'):
+    if sex is 'm':
+        sex = "male"
+    elif sex is 'f':
+        sex = "female"
+    print (sex)
+
+get_gender('m')
+get_gender('f')
+get_gender()
